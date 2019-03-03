70dbc5bf56b13067a548bd3c6c4ae8928664e53f
CQY <793982483@qq.com>
2019-03-03 21:22:34 +0800

    这是实例化的HelloWorld。

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld v = new HelloWorld();
+            v.Hello();
+        }
+    }
+}
