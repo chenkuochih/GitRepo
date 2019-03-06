4ec86c3574bc522aac30b366242b81ddda3a57f4
CQY <793982483@qq.com>
2019-03-06 21:35:40 +0800

    新的方式生成二维码，还添加了多种样式和下载二维码到本地。

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.二维码另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.message_lable = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(306, 88);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 244);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.二维码另存为ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(169, 28);
+            // 
+            // 二维码另存为ToolStripMenuItem
+            // 
+            this.二维码另存为ToolStripMenuItem.Name = "二维码另存为ToolStripMenuItem";
+            this.二维码另存为ToolStripMenuItem.Size = new System.Drawing.Size(168, 24);
+            this.二维码另存为ToolStripMenuItem.Text = "二维码另存为";
+            this.二维码另存为ToolStripMenuItem.Click += new System.EventHandler(this.二维码另存为ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("幼圆", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(187, 219);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "另存为";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("幼圆", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(35, 219);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "生成";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("幼圆", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox1.Location = new System.Drawing.Point(35, 88);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 36);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "请输入长度小于30的字节";
+            this.textBox1.Click += new System.EventHandler(this.textbox1_click);
+            // 
+            // message_lable
+            // 
+            this.message_lable.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.message_lable.AutoSize = true;
+            this.message_lable.Font = new System.Drawing.Font("幼圆", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.message_lable.ForeColor = System.Drawing.Color.Red;
+            this.message_lable.Location = new System.Drawing.Point(32, 135);
+            this.message_lable.Name = "message_lable";
+            this.message_lable.Size = new System.Drawing.Size(0, 15);
+            this.message_lable.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(605, 379);
+            this.Controls.Add(this.message_lable);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "二维码生成";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 二维码另存为ToolStripMenuItem;
+        private System.Windows.Forms.Label message_lable;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ThoughtWorks.QRCode;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sampleText = textBox1.Text;
+            if (sampleText.Length > 30)
+            {
+                message_lable.Text = "字符长度大于30，请重新输入！";
+            }
+            else if (sampleText.Length == 0)
+            {
+                message_lable.Text = "未输入任何字符，请重新输入！";
+            }
+            else
+            {
+                if (textBox1.ForeColor == Color.Black)
+                {
+                    message_lable.Text = "";
+                    if (!string.IsNullOrEmpty(textBox1.Text.Trim()))
+                    {
+                        string enCodeString = textBox1.Text.Trim();
+                        QRCodeEncoder codeEncoder = new QRCodeEncoder();
+                        pictureBox1.Image = codeEncoder.Encode(enCodeString, Encoding.UTF8);
+                    }                  
+                }
+                else
+                {
+                    message_lable.Text = "未输入任何字符，请重新输入！";
+                    textBox1.Text = "";
+                }
+            }
+        }
+
+        private void 二维码另存为ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1 .Image!=null)
+            {
+                SaveFileDialog s = new SaveFileDialog();
+                s.Title = "保存二维码图片";
+                s.Filter = "图片文件(*.jpg)|*.jpg";
+                if (s.ShowDialog()==DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(s.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    MessageBox.Show("保存成功");
+                }
+            }
+        }
+
+        string filename = string.Empty;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog p = new OpenFileDialog();
+            p.Title = "请选择二维码图片";
+            p.Filter = "图片格式(*.jpg)|*.jpg";
+            p.Multiselect = false;
+            if (p.ShowDialog()==DialogResult.OK)
+            {
+                filename = p.FileName;
+                ss();
+            }
+        }
+
+        private void ss()
+        {
+            pictureBox1.Image = new Bitmap(filename);
+            QRCodeDecoder qrDecoder = new QRCodeDecoder();
+            string msg= qrDecoder.decode(new QRCodeBitmapImage(new Bitmap(pictureBox1.Image)), Encoding.UTF8);
+            textBox1.Text = msg;
+            MessageBox.Show("解析完成");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textbox1_click(object sender, EventArgs e)
+        {
+            if(message_lable.Text.Length == 0)
+            {
+                textBox1.Text = "";
+            }
+            pictureBox1.Image = null;//单击文本框二维码消失
+            textBox1.ForeColor = Color.Black;//为了让文本框的文本颜色与注释不相同
+            message_lable.Text = "";
+        }
+    }
+}
