7f00a39ac0fb7ffaf71f9297ada15544620aa541
CQY <793982483@qq.com>
2019-03-12 15:50:26 +0800

    钢琴的实验，实现了缩放功能

---
+++ Sanford.Multimedia.Midi-master/Demo/SequencerDemo/Form1.cs
@@ -0,0 +1,359 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Sanford.Multimedia.Midi;
+using Sanford.Multimedia.Midi.UI;
+
+namespace SequencerDemo
+{
+    public partial class Form1 : Form
+    {
+        private bool scrolling = false;
+
+        private bool playing = false;
+
+        private bool closing = false;
+
+        private OutputDevice outDevice;
+
+        private int outDeviceID = 0;
+
+        private OutputDeviceDialog outDialog = new OutputDeviceDialog();
+
+        private float X;//当前窗体的宽度
+        private float Y;//当前窗体的高度
+
+        public Form1()
+        {
+            InitializeComponent();
+            X = this.Width;
+            Y = this.Height;
+            setTag(this);
+            Form1_Resize(new object(), new EventArgs());
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if(OutputDevice.DeviceCount == 0)
+            {
+                MessageBox.Show("No MIDI output devices available.", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Close();
+            }
+            else
+            {
+                try
+                {
+                    outDevice = new OutputDevice(outDeviceID);
+
+                    sequence1.LoadProgressChanged += HandleLoadProgressChanged;
+                    sequence1.LoadCompleted += HandleLoadCompleted;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                    Close();
+                }
+            }
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            pianoControl1.PressPianoKey(e.KeyCode);
+
+            base.OnKeyDown(e);
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            pianoControl1.ReleasePianoKey(e.KeyCode);
+
+            base.OnKeyUp(e);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            closing = true;
+
+            base.OnClosing(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            sequence1.Dispose();
+
+            if(outDevice != null)
+            {
+                outDevice.Dispose();
+            }
+
+            outDialog.Dispose();
+
+            base.OnClosed(e);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openMidiFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openMidiFileDialog.FileName;
+                Open(fileName);
+            }
+        }
+
+        public void Open(string fileName)
+        {
+            try
+            {
+                sequencer1.Stop();
+                playing = false;
+                sequence1.LoadAsync(fileName);
+                this.Cursor = Cursors.WaitCursor;
+                startButton.Enabled = false;
+                continueButton.Enabled = false;
+                stopButton.Enabled = false;
+                openToolStripMenuItem.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void outputDeviceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutDialog dlg = new AboutDialog();
+
+            dlg.ShowDialog();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                playing = false;
+                sequencer1.Stop();
+                timer1.Stop();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                playing = true;
+                sequencer1.Start();
+                timer1.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                playing = true;
+                sequencer1.Continue();
+                timer1.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void positionHScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            if(e.Type == ScrollEventType.EndScroll)
+            {
+                sequencer1.Position = e.NewValue;
+
+                scrolling = false;
+            }
+            else
+            {
+                scrolling = true;
+            }
+        }
+
+        private void HandleLoadProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            toolStripProgressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void HandleLoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+            startButton.Enabled = true;
+            continueButton.Enabled = true;
+            stopButton.Enabled = true;
+            openToolStripMenuItem.Enabled = true;
+            toolStripProgressBar1.Value = 0;
+
+            if(e.Error == null)
+            {
+                positionHScrollBar.Value = 0;
+                positionHScrollBar.Maximum = sequence1.GetLength();
+            }
+            else
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+        }
+
+        private void HandleChannelMessagePlayed(object sender, ChannelMessageEventArgs e)
+        {
+            if(closing)
+            {
+                return;
+            }
+
+            outDevice.Send(e.Message);
+            pianoControl1.Send(e.Message);
+        }
+
+        private void HandleChased(object sender, ChasedEventArgs e)
+        {
+            foreach(ChannelMessage message in e.Messages)
+            {
+                outDevice.Send(message);
+            }
+        }
+
+        private void HandleSysExMessagePlayed(object sender, SysExMessageEventArgs e)
+        {
+       //     outDevice.Send(e.Message); Sometimes causes an exception to be thrown because the output device is overloaded.
+        }
+
+        private void HandleStopped(object sender, StoppedEventArgs e)
+        {
+            foreach(ChannelMessage message in e.Messages)
+            {
+                outDevice.Send(message);
+                pianoControl1.Send(message);
+            }
+        }
+
+        private void HandlePlayingCompleted(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void pianoControl1_PianoKeyDown(object sender, PianoKeyEventArgs e)
+        {
+            #region Guard
+
+            if(playing)
+            {
+                return;
+            }
+
+            #endregion
+
+            outDevice.Send(new ChannelMessage(ChannelCommand.NoteOn, 0, e.NoteID, 127));
+        }
+
+        private void pianoControl1_PianoKeyUp(object sender, PianoKeyEventArgs e)
+        {
+            #region Guard
+
+            if(playing)
+            {
+                return;
+            }
+
+            #endregion
+
+            outDevice.Send(new ChannelMessage(ChannelCommand.NoteOff, 0, e.NoteID, 0));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(!scrolling)
+            {
+                positionHScrollBar.Value = Math.Min(sequencer1.Position, positionHScrollBar.Maximum);
+            }
+        }
+
+        ///*对GUI界面中的控件大小、位置进行完善，使之能够随APP界面大小自动调整其自身大小。*/
+
+
+        // <summary>
+        /// 将控件的宽，高，左边距，顶边距和字体大小暂存到tag属性中
+        /// </summary>
+        /// <param name="cons">递归控件中的控件</param>
+        private void setTag(Control cons)
+        {
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ";" + con.Height + ";" + con.Left + ";" + con.Top + ";" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                {
+                    setTag(con);
+                }
+            }
+        }
+
+        private void setControls(float newx, float newy, Control cons)
+        {
+            //遍历窗体中的控件，重新设置控件的值
+            foreach (Control con in cons.Controls)
+            {
+                //获取控件的Tag属性值，并分割后存储字符串数组
+                if (con.Tag != null)
+                {
+                    string[] mytag = con.Tag.ToString().Split(new char[] { ';' });
+                    //根据窗体缩放的比例确定控件的值
+                    con.Width = Convert.ToInt32(System.Convert.ToSingle(mytag[0]) * newx);//宽度
+                    con.Height = Convert.ToInt32(System.Convert.ToSingle(mytag[1]) * newy);//高度
+                    con.Left = Convert.ToInt32(System.Convert.ToSingle(mytag[2]) * newx);//左边距
+                    con.Top = Convert.ToInt32(System.Convert.ToSingle(mytag[3]) * newy);//顶边距
+                    Single currentSize = System.Convert.ToSingle(mytag[4]) * newy;//字体大小
+                    con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                    if (con.Controls.Count > 0)
+                    {
+                        setControls(newx, newy, con);
+                    }
+                }
+            }
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            float newx = (this.Width) / X; //窗体宽度缩放比例
+            float newy = (this.Height) / Y;//窗体高度缩放比例
+            setControls(newx, newy, this);//随窗体改变控件大小
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Resize += new EventHandler(Form1_Resize);
+        }
+
+    }
+}
